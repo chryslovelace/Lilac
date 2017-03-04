5601d928de35a8f6a7209f03d8531fb9dd5ed926
Chrys Lovelace <chryslovelace@gmail.com>
2017-03-04 16:24:59 -0500

    more lexer refactoring

--- Lilac/Parser/Lexer.cs
+++ Lilac/Parser/Lexer.cs
@@ -8,67 +8,78 @@ namespace Lilac.Parser
 {
     public class Lexer : ILexer
     {
-        private List<TokenDefinition> TokenDefinitions { get; }
 
         public Lexer(ITokenDefiner tokenDefiner)
         {
             TokenDefinitions = tokenDefiner.GetTokenDefinitions().OrderBy(def => def.Priority).ToList();
             TabWidth = 4;
+            Regex = new Regex(string.Join("|",
+                TokenDefinitions.Select(td => $"(?<{td.TokenType}>{td.Regex})")) + "|(?<Unrecognized>.)",
+                RegexOptions.ExplicitCapture);
         }
 
+        private List<TokenDefinition> TokenDefinitions { get; }
         public int TabWidth { get; set; }
         private Regex Regex { get; set; }
         private int Line { get; set; }
         private Stack<int> Indentations { get; set; }
 
-        private Token NewToken(TokenType type, string content)
+        private Token NewToken(TokenType type, string content, int column = 0)
         {
-            return new Token {TokenType = type, Content = content, Column = 0, Line = Line};
-        }
-        private Token NewToken(TokenType type, string content, int column)
-        {
-            return new Token {TokenType = type, Content = content, Column = column, Line = Line};
+            return new Token { TokenType = type, Content = content, Column = column, Line = Line };
         }
 
         public IEnumerable<Token> Tokenize(TextReader reader)
         {
-            Indentations = new Stack<int>(new []{0});
+            Indentations = new Stack<int>();
+            Indentations.Push(0);
             Line = 0;
-            Regex = new Regex(string.Join("|",
-                TokenDefinitions.Select(td => $"(?<{td.TokenType}>{td.Regex})")) + "|(?<Unrecognized>.)",
-                RegexOptions.ExplicitCapture);
 
             yield return NewToken(TokenType.OpenGroup, "bof");
 
+            foreach (var token in TokenizeLines(reader))
+                yield return token;
+
+            foreach (var token in CloseHangingIndents())
+                yield return token;
+
+            yield return NewToken(TokenType.CloseGroup, "eof");
+        }
+
+        private IEnumerable<Token> TokenizeLines(TextReader reader)
+        {
             string line;
             while ((line = reader.ReadLine()) != null)
             {
+                ++Line;
                 foreach (var token in TokenizeLine(line))
                     yield return token;
             }
+        }
 
+        private IEnumerable<Token> CloseHangingIndents()
+        {
             while (Indentations.Count > 1)
             {
                 Indentations.Pop();
                 yield return NewToken(TokenType.CloseGroup, "dedent");
                 yield return NewToken(TokenType.Newline, string.Empty);
             }
-
-            yield return NewToken(TokenType.CloseGroup, "eof");
         }
-        
+
 
         private IEnumerable<Token> TokenizeLine(string line)
         {
-            ++Line;
+            if (string.IsNullOrWhiteSpace(line))
+                yield break;
 
-            if (string.IsNullOrWhiteSpace(line)) yield break;
-
-            foreach (var token in GetIndentationTokens(line)) yield return token;
+            foreach (var token in GetIndentationTokens(line))
+                yield return token;
 
             var trimmed = line.Trim();
 
-            foreach (var token in GetLineTokens(trimmed)) yield return token;
+            foreach (var token in GetLineTokens(trimmed))
+                yield return token;
 
             yield return NewToken(TokenType.Newline, string.Empty, trimmed.Length + Indentations.Peek());
         }
@@ -83,9 +94,7 @@ namespace Lilac.Parser
         private TokenDefinition GetTokenDefinition(Match match)
         {
             if (match.Groups["Unrecognized"].Success)
-            {
                 throw new SyntaxException($"Unrecognized token '{match.Value}'.");
-            }
             return TokenDefinitions.Find(definition => match.Groups[definition.TokenType.ToString()].Success);
         }
 
@@ -94,8 +103,9 @@ namespace Lilac.Parser
             var indent = line.TakeWhile(char.IsWhiteSpace).Sum(c => c == '\t' ? TabWidth : 1);
             var currentIndent = Indentations.Peek();
 
-            if (indent == currentIndent) yield break;
-            
+            if (indent == currentIndent)
+                yield break;
+
             if (indent > currentIndent)
             {
                 Indentations.Push(indent);
